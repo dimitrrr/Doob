59e6ea3bd425e126bb381c5397dfb85a4885bfcb
dimitrrrr <dima_romanchuk@ukr.net>
2021-07-10 16:20:55 +0300

    beta released

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector('.more a').onclick = (e) => {
+  e.preventDefault();
+}
+
+document.querySelector('.title__button .btn').onclick = (e) => {
+  e.preventDefault();
+}
+
+document.querySelector('form .btn').onclick = (e) => {
+  e.preventDefault();
+}
+
+document.querySelector('.icon').onclick = () => {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
